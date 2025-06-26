d29347ec464cf36cc1a9ad2cd2fdc183d59e95f1
koulibali <dramanekoulibali@outlook.com>
2025-06-26 21:16:07 +0000

    premiere version

---
+++ script.js
@@ -0,0 +1,30 @@
+const submitButton = document.getElementById('submitButton');
+let emailInput = document.getElementById('email');
+let errorMessage = document.getElementById('right');
+
+let container = document.querySelector('.container');
+let thanksPage = document.getElementById('thanksPage');
+let email = emailInput.value;
+
+submitButton.addEventListener('click', function (){
+    email = emailInput.value;
+    let validEmail = email.includes('@');
+    if (validEmail){
+        emailMessage.innerText = emailInput.value;
+        container.style.display = 'none';
+        thanksPage.style.display = 'block';
+
+    }else {
+        emailInput.style.backgroundColor = 'rgba(255, 0, 0, 0.2)';
+        errorMessage.style.display = 'block';
+    }
+})
+
+let dismiss = document.getElementById('dismiss');
+let emailMessage = document.getElementById('emailMessage');
+
+dismiss.addEventListener('click', function (){
+    container.style.display = 'block';
+    container.style.display = 'flex';
+    thanksPage.style.display = 'none';
+})
